1e8838b338ec742ff592db270f5f3a3042dfd907
tanziljws <141249594+tanziljws@users.noreply.github.com>
2025-11-19 22:03:28 +0700

    Update storage/app/public .gitignore and add new files

---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/About.css';
+
+const About = () => {
+  return (
+    <div className="about-page">
+      {/* Konten baru akan ditambahkan di sini */}
+    </div>
+  );
+};
+
+export default About;
