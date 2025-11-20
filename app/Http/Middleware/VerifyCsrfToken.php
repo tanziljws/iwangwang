3ebb4dcc96f86c80696c17a3f7b1d104a53cb7d2
tanziljws <141249594+tanziljws@users.noreply.github.com>
2025-11-20 21:14:55 +0700

    Exclude storage and media routes from CSRF verification

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
 class VerifyCsrfToken extends Middleware
 {
     protected $except = [
-        //
+        'storage/*',
+        'media/*',
     ];
 }
